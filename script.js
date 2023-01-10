0396488f2f7ae70279f2991b8af8fcd178406edd
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-01-09 19:12:19 -0600

    Added data attributes to html and code to get input from local storage

--- script.js
+++ script.js
@@ -57,7 +57,11 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  
+  $('.description').each(function() {
+    var storedHour = $(this).attr('data-hour');
+    var savedCode = localStorage.getItem(storedHour);
+    $(this).text(JSON.parse(savedCode));
+  })
   
  
   // TODO: Add code to display the current date in the header of the page.

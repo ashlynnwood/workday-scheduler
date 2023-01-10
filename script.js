c3a2e024b360d59ab31e4a73e20fd9811b9630e7
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-01-09 19:09:06 -0600

    Debugged colorTime function and changed timeblock ids to only numbers

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
+// Global Variables
+var saveBtn = $('.saveBtn');
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-
-// Global Variables
-// var workInput = $(".description");
-var saveBtn = $('.saveBtn');
-
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -34,9 +32,12 @@ $(function () {
   
   function colorTime() {
     var currentHour = dayjs().hour();
+    // Check to see if accurately grabbing current hour
+    console.log(currentHour);
     $('.time-block').each(function() {
       var timeBlock = parseInt($(this).attr('id'));
-
+      // Check to see if accurately grabbing timeblock
+        console.log(timeBlock);
       if (currentHour > timeBlock) {
         $(this).addClass('past').removeClass('present future');
       
@@ -49,12 +50,15 @@ $(function () {
     });
   
   }
+  
+  // Call colortime function
   colorTime();
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  
+  
  
   // TODO: Add code to display the current date in the header of the page.
   var today = dayjs();
